3bf606afc02c3381c735b7785fd076c95d5219d1
Drekken <drekken.pownz@protonmail.me>
2025-06-28 19:40:53 -0400

    feat: add map configuration system with default and available map settings

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration for map settings
+
+# Default map name. Change this value to switch the default map easily.
+DEFAULT_MAP = "LightShade_Pathing_1"
+
+# Optional: list of available maps
+AVAILABLE_MAPS = [
+    "LightShade_Pathing_0",
+    "LightShade_Pathing_1",
+
+]
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import aiohttp
 
 import argparse
 import sc2
+import config
 
 from sc2.main import run_game
 from sc2.data import Race, Difficulty
@@ -30,8 +31,8 @@ def parse_arguments():
                         help="Computer race. One of [Terran, Zerg, Protoss, Random]. Default is Terran. Only for local play.")
     parser.add_argument("--ComputerDifficulty", type=str, default="VeryEasy",
                         help=f"Computer difficulty. One of [VeryEasy, Easy, Medium, MediumHard, Hard, Harder, VeryHard, CheatVision, CheatMoney, CheatInsane]. Default is VeryEasy. Only for local play.")
-    parser.add_argument("--Map", type=str, default="LightShade_Pathing_1",
-                        help="Custom map to use: LightShade_Pathing_0. ")
+    parser.add_argument("--Map", type=str, default=config.DEFAULT_MAP,
+                        help="Custom map to use.")
 
     # Both Ladder and Local play arguments
     parser.add_argument("--OpponentId", type=str, help="A unique value identifying opponent.")
